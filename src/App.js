208e650ca6f384fa44e5c2a5e8d5c1bc8f283d0e
chandra prakash <chandraprakash.tiwari@easebuzz.in>
2021-02-14 14:11:48 +0530

    create movie list project in react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import 'antd/dist/antd.css';
+import { Cards } from './Components/Cards/Cards';
+import { Layout,Row,Col } from 'antd';
+import FooterLayout from './Components/Footer';
+import HeaderLayout from './Components/Header'
+import JsonData from './Services/JsonData/sample';
+import CommanHeader from './Components/SharedComponent';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const { Header,Content, Footer } = Layout;
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      search: null,
+      JsonDataDataList: JsonData.entries,
+      sortType: 'desc',
+      error: null,
+      isLoaded: false,
+    };
+  }
+  onSearch = (event) => {
+    let keyword = event.target.value;
+    this.setState({ search: keyword })
+  }
+  handleMenuClick = (e) => {
+    let clickEvent = e.key;
+    this.setState({
+      sortType: clickEvent
+    })
+  }
+  render() {
+    const { JsonDataDataList, sortType,error, isLoaded, items } = this.state;
+    const sorted = JsonDataDataList.sort((a, b) => {
+      if (sortType === "asc") {
+        return a.title.localeCompare(b.title)
+      }
+      else {
+        return b.title.localeCompare(a.title)
+      }
+    })
+    const JsonDataData = sorted.filter((data) => {
+      if (this.state.search == null)          
+        return data
+      else if (data.title.toLowerCase().includes(this.state.search.toLowerCase())) {
+        return data
+      }
+    }).map(data => {
+      return (        
+        <Cards title={data.title} imageUrl={data.images['Poster Art'].url} />
+      )
+    })
+    return (
+      <div className="App">
+        <Layout className="layout">
+          <Header>
+            <HeaderLayout/>
+          </Header>
+          <Row className="Second-Header">
+            <Col xs={24}>
+                    <h1>Popular Movies</h1>
+                </Col>
+          </Row>
+          <Content style={{ padding: '0 50px' }}>          
+            <CommanHeader/>
+            <div className="site-layout-content">
+              <Row>
+                {JsonDataData}
+              </Row>
+            </div>
+          </Content>
+          <Footer className="footerArea">
+            <FooterLayout/>
+          </Footer>
+        </Layout>
+      </div>
+    )
+  }
 }
-
 export default App;
